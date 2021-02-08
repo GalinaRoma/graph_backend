28ca885e849c6e9e0ac760b575ef5452e69b1b75
GalinaRomakhina <galinaroma51@gmail.com>
2021-02-08 22:37:55 +0500

    Fixes for hacker test

--- add_hacker.py
+++ add_hacker.py
@@ -5,8 +5,9 @@ if __name__ == '__main__':
         data = json.load(f)
         data['data'].append({
             'id': 'hacker1',
-            'name': 'Hacker',
+            'name': 'Unknown',
             'type': 'host',
+            "created_at": "2021-02-08T12:10:22.618Z",
             'networks': [{
                 'ip': '10.10.4.10',
                 'mask': '255.255.255.252',
@@ -19,7 +20,9 @@ if __name__ == '__main__':
         data['data'].append({
             "id": 'hacker1-h8',
             "first_interfaces_group_id": 'hacker1',
-            "second_interfaces_group_id": 'h8'
+            "second_interfaces_group_id": 'h8',
+            "is_approved": False,
+            "protocols": ['ping', 'telnet', 'ssh', 's7comm']
         })
     with open('edges.json', 'w', encoding='utf-8') as f:
         json.dump(data, f)
