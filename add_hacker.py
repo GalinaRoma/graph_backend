445ba36d255cc36055421d2b75e44288c5260b9c
GalinaRomakhina <galinaroma51@gmail.com>
2021-02-19 16:54:44 +0500

    Fix add_hacker

--- add_hacker.py
+++ add_hacker.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
             'name': 'Unknown',
             'type': 'host',
             "created_at": "2021-02-09T12:10:22.618Z",
-            'networks': [{
-                'ip': '10.10.4.10',
-                'mask': '255.255.255.252',
+            'interfaces': [{
+                'ip': '10.10.2.50',
+                'mask': '255.255.255.0',
             }]
         })
     with open('devices.json', 'w', encoding='utf-8') as f:
@@ -18,11 +18,18 @@ if __name__ == '__main__':
     with open('edges.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
         data['data'].append({
-            "id": 'hacker1-h8',
+            "id": 'hacker1-plc2',
             "first_interfaces_group_id": 'hacker1',
-            "second_interfaces_group_id": 'h8',
+            "second_interfaces_group_id": 'plc2',
             "is_approved": False,
-            "protocols": ['ping', 'telnet', 'ssh', 's7comm']
+            "protocols": ['ICMP', 'telnet', 'SSH', 's7comm']
+        })
+        data['data'].append({
+            "id": 'hacker1-s3',
+            "first_interfaces_group_id": 'hacker1',
+            "second_interfaces_group_id": 's3',
+            "is_approved": False,
+            "protocols": ['ICMP', 'telnet', 'SSH', 's7comm']
         })
     with open('edges.json', 'w', encoding='utf-8') as f:
         json.dump(data, f)
